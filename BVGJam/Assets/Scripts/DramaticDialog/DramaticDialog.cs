ccc970e45113ab26eec04743ffcf298948cfa66f
fellenex <outcasthat@gmail.com>
2023-03-07 21:25:27 -0500

    Huge cleanup in DramaticDialog.cs. Refactored most of it into Colours.json. StoryTriggers basically useless now

--- BVGJam/Assets/Scripts/DramaticDialog/DramaticDialog.cs
+++ BVGJam/Assets/Scripts/DramaticDialog/DramaticDialog.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public static class DramaticDialog {
 
@@ -9,135 +10,29 @@ public static class DramaticDialog {
     //Store arrays of strings instead of a single string so that we can display things on multiple lines
     public static Dictionary<string, string[]> mapTriggerToTexts;
 
-    private static Color redGood;
-    private static Color redBad;
-    private static Color blueGood;
-    private static Color blueBad;
-    private static Color yellowGood;
-    private static Color yellowBad;
-    private static Color purpleGood;
-    private static Color purpleBad;
-    private static Color greenGood;
-    private static Color greenBad;
 
-    public static void setupDramaticDialogs() {
-        redGood = new Color(248,25,0,255);
-        redBad = new Color(199,120,120,255);
-        blueGood = new Color(11,3,252,255);
-        blueBad = new Color(148,183,194,255);
-        yellowGood = new Color(255,255,51,255);
-        yellowBad = new Color(219,224,182,255);
-        purpleGood = new Color(177,0,231,255);
-        purpleBad = new Color(99,33,133,255);
-        greenGood = new Color(0,232,31,255);
-        greenBad = new Color(209,237,119,255);
+    public static Dictionary<ValueTuple<String, String>,Color> DramaticDialogColours = new System.Collections.Generic.Dictionary<ValueTuple<String, String>,Color>{
+        {("red", "good"), new Color(248,25,0,255)},
+        {("red", "bad"), new Color(199,120,120,255)},
+        {("blue", "good"), new Color(11,3,252,255)},
+        {("blue", "bad"), new Color(148,183,194,255)},
+        {("yellow", "good"), new Color(255,255,51,255)},
+        {("yellow", "bad"), new Color(219,224,182,255)},
+        {("purple", "good"), new Color(177,0,231,255)},
+        {("purple", "bad"), new Color(99,33,133,255)},
+        {("green", "good"), new Color(0,232,31,255)},
+        {("green", "bad"), new Color(209,237,119,255)}
+    };
 
-        mapTriggerToColor = new Dictionary<string, Color>();
-        mapTriggerToTexts = new Dictionary<string, string[]>();
-
-        setupColorMappings();
-        setupTextMappings();
-
+    public static Color getColorByTrigger(Conversation_Trigger _trigger) {
+        return DramaticDialogColours[(_trigger.colour, _trigger.quality)];
     }
 
-    public static void setupColorMappings() {
-        //Blue mappings
-        mapTriggerToColor.Add(StoryTriggers.getJusticeSweat, blueGood);
-        mapTriggerToColor.Add(StoryTriggers.getBardsRequiem, blueGood);
-        //
-        mapTriggerToColor.Add(StoryTriggers.getToxicSweat, blueBad);
-        mapTriggerToColor.Add(StoryTriggers.getExistentialSweat, blueBad);
-
-        //Purple mappings        
-        mapTriggerToColor.Add(StoryTriggers.getClericsBlessing, purpleGood);
-        mapTriggerToColor.Add(StoryTriggers.getClericsMourning, purpleGood);
-        //
-        mapTriggerToColor.Add(StoryTriggers.getEvilAura, purpleBad);
-        mapTriggerToColor.Add(StoryTriggers.getCursedAura, purpleBad);
-
-        //Red mappings
-        mapTriggerToColor.Add(StoryTriggers.getPaladinBloodGood, redGood);
-        mapTriggerToColor.Add(StoryTriggers.getYourBlood, redGood);
-        mapTriggerToColor.Add(StoryTriggers.getAssassinsBlood, redGood);
-        //
-        mapTriggerToColor.Add(StoryTriggers.getPaladinBloodBad, redBad);
-        
-        //Yellow mappings
-        mapTriggerToColor.Add(StoryTriggers.getYellowGood, yellowGood);
-        mapTriggerToColor.Add(StoryTriggers.getYellowBad, yellowBad);
-
-        //Green mappings
-        mapTriggerToColor.Add(StoryTriggers.getSonicSpore, greenGood);
-        mapTriggerToColor.Add(StoryTriggers.getLakewaterSpore, greenGood);
-        //
-        mapTriggerToColor.Add(StoryTriggers.getCursedSpore, greenBad);
-    }
-
-    public static void setupTextMappings() {
-        //Blue mappings
-        mapTriggerToTexts.Add(StoryTriggers.getJusticeSweat,
-            new string[]{"You’ve obtained The Paladin’s Toxic Masculinity Sweat!"
-            });
-        mapTriggerToTexts.Add(StoryTriggers.getBardsRequiem,
-            new string[]{"As the sun goes down, on this Blue night.",
-                "A friend cries eternal, tears for the knight.",
-                "A solemn hymn, for a solid him.",
-                "A moment grim, from the Reaper’s whim.",
-                "",
-                "You stomached The Bard’s Requiem!"
-            });
-        //
-        mapTriggerToTexts.Add(StoryTriggers.getToxicSweat, 
-            new string[]{"...I don’t know if I am capable of feeling happiness.",
-                "You received The Paladin’s Existential Sweat!"
-            });
-        mapTriggerToTexts.Add(StoryTriggers.getExistentialSweat,
-            new string[]{"You obtain a few droplets of Jonathan’s profuse J U S T I C E  S W E A T."});
-
-
-        //Purple mappings        
-        mapTriggerToTexts.Add(StoryTriggers.getClericsBlessing,
-            new string[]{"Saint Casey heals you with her Cleric magic.",
-                "You receive "+'"'+"Cleric's Blessing"+'"'+"!",
-                "And also you stop bleeding!"});
-
-
-        mapTriggerToTexts.Add(StoryTriggers.getClericsMourning,
-            new string[]{"Sir Jonathan’s soul rests and finds eternal peace as a Purple tear slides down Saint Casey’s face.",
-                '"'+"Rest easy...friend."+'"',
-                "You obtain Cleric's Mourning"});
-        //
-        mapTriggerToTexts.Add(StoryTriggers.getEvilAura, new string[]{
-            "Saint Casey begins to speak in tongues as darkness suffocates the sky. The clouds are split asunder, as a primordial moan echoes across space and time.",
-            "Saint Casey has broken her holy vow.",
-            "You have obtained an Evil Cleric's Aura!"
-        });
-        mapTriggerToTexts.Add(StoryTriggers.getCursedAura, new string[]{
-            "Saint Casey begins to speak in tongues as darkness suffocates the sky. For a brief moment, Sir Jonathan can be heard screaming "+'"'+"Put me back!"+'"',
-            "Saint Casey has broken her holy vow.",
-            "You have obtained a Cursed Cleric's Aura"
-        });
-
-        /*
-
-        //Red mappings
-        mapTriggerToTexts.Add(StoryTriggers.getPaladinBloodGood, redGood);
-        mapTriggerToTexts.Add(StoryTriggers.getYourBlood, redGood);
-        mapTriggerToTexts.Add(StoryTriggers.getAssassinsBlood, redGood);
-        //
-        mapTriggerToTexts.Add(StoryTriggers.getPaladinBloodBad, redBad);
-        
-        //Yellow mappings
-        mapTriggerToTexts.Add(StoryTriggers.getYellowGood, yellowGood);
-        mapTriggerToTexts.Add(StoryTriggers.getYellowBad, yellowBad);
-
-        //Green mappings
-        mapTriggerToTexts.Add(StoryTriggers.getSonicSpore, greenGood);
-        mapTriggerToTexts.Add(StoryTriggers.getLakewaterSpore, greenGood);
-        //
-        mapTriggerToTexts.Add(StoryTriggers.getCursedSpore, greenBad);
-
-
-        */
-    }
+    //TODO
+    //Read Colours.json for the dramatic dialogs
+    //if no speaker/mood, don't show npc
+    //if speaker/mood, show faded npc
+    //write ColourConversation class
+    //      has an id and a list of Conversation_Statements
+    //      get colourconversation by id
 }
